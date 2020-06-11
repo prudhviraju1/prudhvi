ccadabb773c088cbd0f0fc8c87c42af4af358aa6
Prudhvi <prudhvi.saraswathi@gmail.com>
2020-06-10 22:13:40 -0700

    signup functionality

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,26 @@
-window.onload=function docFunction(){
-// var firstClass=4;
-for(var i=0;i<=4;i++){
-    document.getElementsByClassName("firstClass")[i].style.color="red"
+function signup(){
+    let username = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+
+    var finalResult = `
+        username = ${username},
+        password = ${password}
+    `
+    if(username == ''){
+        alert('please enter username');
+    }
+    else if(password == ''){
+        alert('please enter password');
+    }
+    else{
+        alert(finalResult);
+    }
 }
-}
+
+
+// window.onload=function docFunction(){
+// // var firstClass=4;
+// for(var i=0;i<=4;i++){
+//     document.getElementsByClassName("firstClass")[i].style.color="red"
+// }
+// }
