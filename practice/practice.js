0337c387986cdd3335d6fe1f656f9256a5c03bbb
Prudhvi <prudhvi.saraswathi@gmail.com>
2020-06-10 21:48:12 -0700

    javascript file update

--- practice/practice.js
+++ practice/practice.js
@@ -1,28 +1,6 @@
-<<<<<<< HEAD
-// jhjfdrgjyug
-// giuhejlgs
-// jogshitunesi
-// nksiths
-// jhjfdrgjyug
-// giuhejlgsnkfuihiu
-// jogshitunesi
-// nksiths
-// giuhejlgsfidjspigaod
-// jogshitunesiagkuhuhga
-// nksithskhgaiuhui
-// jhjfdrgjyug
-// giuhejlgsfushiufhhusi
-// jogshitunesi
-// nksiths
-// jhjfdrgjyugfshiuhfshjh
-// giuhejlgs
-// jogshitunesi
-// nksiths
-=======
 window.onload=function docFunction(){
 // var firstClass=4;
 for(var i=0;i<=4;i++){
     document.getElementsByClassName("firstClass")[i].style.color="red"
 }
 }
->>>>>>> 0e6df0f... prudhvi/my_branch
